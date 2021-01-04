9e1b8867f81d8d28034fcebe91384eed59cd0b45
kumar <kumarkhandagle33@gmail.com>
2021-01-04 14:41:26 +0530

    Adding Changes

--- custom_slave_Lite_axi_adder_Multiplier/nexys_4_custom_slave_axi.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_top_mul.elf_on_local.tcl
+++
@@ -1,7 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744993A"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744993A"} -index 0
-dow C:/Users/labview/nexys_4_custom_slave_axi/nexys_4_custom_slave_axi.sdk/top_mul/Debug/top_mul.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744993A"} -index 0
-con
--- custom_slv_io_port/custom_slv_io_port.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_top.elf_on_local.tcl
+++
@@ -1,7 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744993A"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744993A"} -index 0
-dow C:/Users/labview/custom_slv_io_port/custom_slv_io_port.sdk/top/Debug/top.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744993A"} -index 0
-con
